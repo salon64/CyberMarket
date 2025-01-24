30d7ff12da890e5151fb847e224bf1b76cd486cf
malco <56635871+salon64@users.noreply.github.com>
2025-01-24 13:24:12 +0100

    crt screen affects

--- malcolm-test/cybermarket/src/App.tsx
+++ malcolm-test/cybermarket/src/App.tsx
@@ -1,5 +1,27 @@
-import Alert from "./components/Alert";
-import ListGroup from "./components/ListGroup";
+
+import "./App.css";
+
+function App() {
+  return (
+      <div className="crt-screen">
+        <div className="crt-content">
+          <h1>Cyberpunk is like really preem</h1>
+          <p>But retro might be cooler, idk?</p>
+        </div>
+      </div>
+  );
+}
+
+
+
+export default App;
+
+
+
+
+
+// import Alert from "./components/Alert";
+// import ListGroup from "./components/ListGroup";
 
 // function App() {
 //   let devs = ["Malcolm", "shaya", "Olle"];
@@ -10,14 +32,12 @@ import ListGroup from "./components/ListGroup";
 
 //   return <div><ListGroup items={devs} heading="Devs" onSelectItem={handleSelectItem}/></div>;
 // }
-function App() {
-  return (
-    <div>
-      <Alert> 
-        Hello <span>World</span>
-      </Alert>
-    </div>
-  );
-}
-
-export default App;
+// function App() {
+//   return (
+//     <div>
+//       <Alert> 
+//         Hello <span>World</span>
+//       </Alert>
+//     </div>
+//   );
+// }
