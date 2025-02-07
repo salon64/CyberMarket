83f9744c03fb70b55ef32e59496d311ad211d7c8
malco <56635871+salon64@users.noreply.github.com>
2025-02-07 15:35:47 +0100

    loading user items wip

--- frontend/new-react-vite/src/components/Profile/Profile.tsx
+++ frontend/new-react-vite/src/components/Profile/Profile.tsx
@@ -1,36 +1,42 @@
 import React, { useState, useEffect, Component, useRef } from "react";
 import MyImage from "./aswedishtiger.png";
 import "../cyberpunk-css-main/cyberpunk.css";
+
 async function handleSubmit(e) {
   // Prevent the browser from reloading the page
   e.preventDefault();
+
   // Read the form data
-  
   const form = e.target;
   const formData = new FormData(form);
-  console.log(formData)
+  console.log(formData);
+
   // You can pass formData as a fetch body directly:
-  let userID: string = (document.getElementById("nameChangeFormID") as HTMLInputElement).value;
-  userID = "http://ronstad.se/users/" + userID
+  let userID: string = (
+    document.getElementById("nameChangeFormID") as HTMLInputElement
+  ).value;
+  userID = "http://ronstad.se/users/" + userID;
   console.log(userID);
+
   // Or you can work with it as a plain object:
   const formJson = Object.fromEntries(formData.entries());
   console.log(JSON.stringify(formJson));
-  fetch(userID, { method: "POST", body: JSON.stringify(formJson)})
-  .then(response => {
-    if (response.ok === true){
-      console.log("Valid")
-      alert("Account has been succesfully updated");
-    }
-    else {
-      console.log("Invalid")
-      alert("nuh uh");
-    }})
-  .then(data => console.log(data));
+  fetch(userID, { method: "POST", body: JSON.stringify(formJson) })
+    .then((response) => {
+      if (response.ok === true) {
+        console.log("Valid");
+        alert("Account has been succesfully updated");
+      } else {
+        console.log("Invalid");
+        alert("nuh uh");
+      }
+    })
+    .then((data) => console.log(data));
   fetch("http://ronstad.se/users", { method: "GET" })
-  .then((response) => response.json())
-  .then(res => console.log(res))
+    .then((response) => response.json())
+    .then((res) => console.log(res));
 }
+
 function Profile() {
   const nameForm = useRef(null);
   const [data, setData] = useState(null);
@@ -39,22 +45,32 @@ function Profile() {
     fetch("http://ronstad.se/users", { method: "GET" })
       .then((response) => response.json())
       .then((data) => setData(data))
-      .then((data) => console.log(data))
+      // .then((data) => console.log(data))
+      .then(data => console.log(data))
       .catch((error) => console.error("Error: ", error));
 
-      var tmp = JSON.stringify(data)
-      console.log(tmp)
-    }, []);
+    // console.log(JSON.stringify(data));
+    console.log("seal was here")
+  }, []);
+
+
+  const [userItems, setUserItems] = useState(null);
+  useEffect(() => {
+    fetch("http://ronstad.se/users", {method: "GET2"})
+    .then((response) => response.json())
+    .then(useritems => setUserItems(useritems))
+    .then(userItems => console.log(userItems))
+    .catch(error => console.error("Error2: ", error))
+  })
 
   // const handleClickEvent = () => {
 
-    
   // };
-  // function myFunction(event: React.FormEvent<HTMLFormElement>) { 
-  //   event.preventDefault(); 
-  //   const form = event.currentTarget; 
+  // function myFunction(event: React.FormEvent<HTMLFormElement>) {
+  //   event.preventDefault();
+  //   const form = event.currentTarget;
   //   const input = form.elements.namedItem('q') as HTMLInputElement;
-  //   console.log(input.value); 
+  //   console.log(input.value);
   // }
 
   return (
@@ -66,27 +82,43 @@ function Profile() {
           <img src={MyImage} alt="User data"></img>
         )}
       </div>
-      
+
       <div>
-          <div className="cyber-input">
-            
-            <input type="text" placeholder="Enter ID..." id="nameChangeFormID"></input>
-            <form method="post" onSubmit={handleSubmit}>
+        {userItems ? (
+          JSON.stringify(data) ) : (
+            "could not laod user items"
+        )}
+      </div>
+
+      <div>
+        <div className="cyber-input">
+          <input
+            type="text"
+            placeholder="Enter ID..."
+            id="nameChangeFormID"
+          ></input>
+          <form method="post" onSubmit={handleSubmit}>
             <label>
-            <input name="token" type="text" placeholder="token..."/>
+              <input name="token" type="text" placeholder="token..." />
             </label>
             <label>
-            <input name="new_name" type="text" placeholder="Enter new username..."/>
+              <input
+                name="new_name"
+                type="text"
+                placeholder="Enter new username..."
+              />
             </label>
             <br></br>
             <label>
-            <input name="new_pswd" type="password" placeholder="Enter new password..."/>
+              <input
+                name="new_pswd"
+                type="password"
+                placeholder="Enter new password..."
+              />
             </label>
-          <button>Save changes</button>
+            <button>Save changes</button>
           </form>
-          </div>
-
-
+        </div>
       </div>
 
       {/* <div className="cyber-input">
