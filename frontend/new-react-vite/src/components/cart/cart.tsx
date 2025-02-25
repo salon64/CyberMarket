aefe5c9d8b76db9a2935990067f46ecd91f45464
PelleGH <shayarezaiyazdi@gmail.com>
2025-02-25 20:12:32 +0100

    cart draft

--- frontend/new-react-vite/src/components/cart/cart.tsx
+++ frontend/new-react-vite/src/components/cart/cart.tsx
@@ -1,7 +1,34 @@
-import "../Marketplace/Marketplace.css";
+import "./cart.css";
 function Cart(){
     return (
-    <header>Shopping cart</header>
+    <div className="right">      <table className="cyber-table store-table ac-custom">
+    <thead>
+      <tr className="thead">
+        <th>Item</th>
+        <th>Price</th>
+        <th>Description</th>
+        <th>Seller</th>
+        <th>Remove</th>
+      </tr>
+    </thead>
+    <tbody>
+        <tr className="">
+            <td className="">{}(ItemPlaceholder)</td>
+            <td className="">{}(PricePlaceholder)</td>
+            <td className="">{}(DescriptionPlaceholder)</td>
+            <td className="">{}<image className="sellIcon"></image></td>
+            <td className="">{}<button className="rm-button">X</button></td>
+        </tr>
+        <tr className="">
+            <td>Total:</td>
+            <td className="">{}</td>
+        </tr>
+      
+    </tbody>
+    </table>
+    <button>Checkout</button>
+    </div>
+   
 
     );
 }
