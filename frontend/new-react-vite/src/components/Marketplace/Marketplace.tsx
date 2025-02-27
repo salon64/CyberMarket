bea498d21d7603fdfd78796abf412bc93aaea43f
PelleGH <shayarezaiyazdi@gmail.com>
2025-02-27 13:38:10 +0100

    buy fix

--- frontend/new-react-vite/src/components/Marketplace/Marketplace.tsx
+++ frontend/new-react-vite/src/components/Marketplace/Marketplace.tsx
@@ -7,6 +7,9 @@ interface MarketplaceState {
   sortBy: string;
   search: string;
 }
+interface userIDInt {
+  UserID: number
+}
 
 interface MarketplaceItems {
   ItemID: number;
@@ -16,7 +19,7 @@ interface MarketplaceItems {
   ItemName: string;
   ItemDescription: string;
   ImgURL: string;
-
+  
   OfferID: number;
   Price: number;
   CreationDate: string;
@@ -28,7 +31,7 @@ interface MarketplaceItems {
 
 
 function Marketplace() {
-  const [sortState, setSortState] = useState<MarketplaceState>({ sortBy: "Newest", search: "" })
+  const [sortState, setSortState] = useState<MarketplaceState>({sortBy: "Newest", search: ""})
   const [marketplaceItems, setMarketplaceItems] = useState<MarketplaceItems[]>([]);
   onchange = s => { // <-- wtf is this
     console.log(sortState.sortBy)
@@ -36,22 +39,27 @@ function Marketplace() {
   }
 
   useEffect(() => {
-    var fetchString = `http://ronstad.se:5687/Marketplace/displayMarket`
-    fetch(fetchString, { method: "POST", body: JSON.stringify(sortState) }) // Replace with your actual API URL
-      .then((response) => response.json())
-      .then((marketplaceItems) => setMarketplaceItems(marketplaceItems))
-      .catch((error) => console.error("Error: ", error));
+    var fetchString = `http://ronstad.se/Marketplace/displayMarket`
+    fetch(fetchString, { method: "POST", body:  JSON.stringify(sortState)}) // Replace with your actual API URL
+            .then((response) => response.json())
+            .then((marketplaceItems) => setMarketplaceItems(marketplaceItems))
+            .catch((error) => console.error("Error: ", error));
   }, [sortState])
 
   const buyItem = (item: MarketplaceItems) => {
     console.log("OfferID: ?", item.OfferID);
-    const jsonItem = JSON.stringify(item)
-    var fetchString = `http://ronstad.se:5687/Marketplace/buy/` + localStorage.getItem("uid")
-
-    fetch(fetchString, { method: "POST", body: jsonItem })
-      .then((response) => response.json())
-      .then((marketplaceItems) => setMarketplaceItems(marketplaceItems))
-      .catch((error) => console.error("Error: ", error));
+    var tmpInt: userIDInt = {UserID: Number(localStorage.getItem("uid"))}
+    const jsonItem = JSON.stringify(tmpInt)
+    console.log(tmpInt)
+    console.log(jsonItem)
+    var fetchString = `http://ronstad.se/Marketplace/buy/` + item.ItemID
+    
+    fetch(fetchString, { method: "POST", body:  jsonItem}) 
+            .then((response) => response.json())
+            .then((marketplaceItems) => {setMarketplaceItems(marketplaceItems)
+              window.location.reload();
+            })
+            .catch((error) => console.error("Error: ", error));
 
   };
 
@@ -73,84 +81,84 @@ function Marketplace() {
     else {
       console.log("not empty")
       return (
-        marketplaceItems.map((item: MarketplaceItems) => (
-          <tr key={item.ItemID}>
-            <td className="">{item.ItemName}</td>
-            <td className="">{item.Price}</td>
-            <td className="">{item.ItemDescription}</td>
-            <td className="">{item.Username}</td>
-            <td>
-              <input onClick={() => buyItem(item)} className='buy-button' type='button' value='Buy' />
-            </td>
-          </tr>))
+          marketplaceItems.map((item: MarketplaceItems) => (
+              <tr key={item.ItemID}>
+                <td className="">{item.ItemName}</td>
+                <td className="">{item.Price}</td>
+                <td className="">{item.ItemDescription}</td>
+                <td className="">{item.Username}</td>
+                <td>
+                  <input onClick={() => buyItem(item)} className='buy-button' type='button' value='Buy' />
+                </td>
+              </tr>))
       )
 
 
 
     }
-  }
+  } 
 
   return (
-
-    <body>
-      <div className="left-right-container">
-        <div className="left">
-          <CyberpunkWindow>
-            <div className="left-elements">
-              <div className="sorting oxanium-font">
-                Sort by:
-                <div className="cyber-input">
-                  <div className="cyber-select ac-purple ">
-                    <select className="oxanium-font" onChange={(s) => setSortState({ sortBy: s.target.value, search: sortState.search })}>
-                      <option style={{ color: "black" }} className="oxanium-font" value="Newest"           >Newest</option>
-                      <option style={{ color: "black" }} className="oxanium-font" value="Oldest"           >Oldest</option>
-                      <option style={{ color: "black" }} className="oxanium-font" value="Price_Ascending"  >Price Ascending</option>
-                      <option style={{ color: "black" }} className="oxanium-font" value="Price_Descending" >Price Descending</option>
-                      <option style={{ color: "black" }} className="oxanium-font" value="Alphabetically_Ascending" >Alphabetically A-Ö</option>
-                      <option style={{ color: "black" }} className="oxanium-font" value="Alphabetically_Descending" >Alphabetically Ö-A</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-
-              <div className="tags">
-
-                <div className="tagtext">
-                  <header>Tags</header>
-                  <ul>
-                    <input type="checkbox" className="cyber-check-tags" ></input> Cyberware <br></br>
-                    <input type="checkbox" className="cyber-check-tags" ></input> Consumables<br></br>
-                    <input type="checkbox" className="cyber-check-tags" ></input> Weapons<br></br>
-                    <input type="checkbox" className="cyber-check-tags" ></input> Drugs<br></br>
-                    <input type="checkbox" className="cyber-check-tags" ></input> Armor<br></br>
-                  </ul>
-                </div>
+    
+  <body>
+    <div className="left-right-container">
+      <div className="left">
+        <CyberpunkWindow>
+        <div className="left-elements">
+          <div className="sorting oxanium-font">
+            Sort by:
+            <div className="cyber-input">
+              <div className="cyber-select ac-purple ">
+                <select className="oxanium-font"  onChange={(s) => setSortState({sortBy: s.target.value, search: sortState.search})}>
+                    <option style={{color:"black"}}className="oxanium-font" value="Newest"           >Newest</option>
+                    <option style={{color:"black"}}className="oxanium-font" value="Oldest"           >Oldest</option>
+                    <option style={{color:"black"}}className="oxanium-font" value="Price_Ascending"  >Price Ascending</option>
+                    <option style={{color:"black"}}className="oxanium-font" value="Price_Descending" >Price Descending</option>
+                    <option style={{color:"black"}}className="oxanium-font" value="Alphabetically_Ascending" >Alphabetically A-Ö</option>
+                    <option style={{color:"black"}}className="oxanium-font" value="Alphabetically_Descending" >Alphabetically Ö-A</option>
+                </select>
               </div>
             </div>
-          </CyberpunkWindow>
+          </div>
 
+          <div className="tags">  
+                    
+            <div className="tagtext">
+            <header>Tags</header> 
+              <ul>
+                <input type="checkbox" className="cyber-check-tags" ></input> Cyberware <br></br>
+                <input type="checkbox" className="cyber-check-tags" ></input> Consumables<br></br>
+                <input type="checkbox" className="cyber-check-tags" ></input> Weapons<br></br>
+                <input type="checkbox" className="cyber-check-tags" ></input> Drugs<br></br>
+                <input type="checkbox" className="cyber-check-tags" ></input> Armor<br></br>
+              </ul>
+            </div>
+          </div>
         </div>
-
-        <div className="right">
-
-          {/* store */}
-          <table className="cyber-table store-table ac-custom">
-            <thead>
-              <tr className="thead">
-                <th>Item</th>
-                <th>Price</th>
-                <th>Description</th>
-                <th>Seller</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {market()}
-            </tbody>
-          </table>
-        </div>
+        </CyberpunkWindow>
+        
       </div>
-    </body>
+
+      <div className="right">
+        
+        {/* store */}
+        <table className="cyber-table store-table ac-custom">
+          <thead>
+            <tr className="thead">
+              <th>Item</th>
+              <th>Price</th>
+              <th>Description</th>
+              <th>Seller</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {market()}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </body>
 
   );
 }
